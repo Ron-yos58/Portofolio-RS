b0453f7f74014dab896eee12190b837237b43b7e
Ronald Sebastian <ronaldsebastian58@gmail.com>
2026-09-21 14:48:04 +0700

    Refactor code structure for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#f97316',
-        dark: '#0f172a',
-        secondary: '#64748b'
+        primary: "#f97316",
+        dark: "#0f172a",
+        secondary: "#64748b",
       },
-    screens: {
-      '2xl': '1320px'
-    },
+      screens: {
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
+};
